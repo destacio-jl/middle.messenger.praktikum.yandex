90fc1db2236a207e10ad3f3949d9cc62999ef798
Dmitriy Chernov <destacio-jl@yandex.ru>
2022-01-15 19:58:21 +0600

    add express and routes

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+
+const routes = [
+  {
+    path: '/',
+    file: '/login/login.html',
+  },
+  {
+    path: '/login',
+    file: '/login/login.html',
+  },
+  {
+    path: '/main',
+    file: '/main/main.html',
+  },
+  {
+    path: '/profile',
+    file: '/profile/profile.html',
+  },
+  {
+    path: '/change-profile',
+    file: '/profile/changeProfile.html',
+  },
+  {
+    path: '/change-password',
+    file: '/profile/changePassword.html',
+  },
+  {
+    path: '/signin',
+    file: '/signin/signin.html',
+  },
+]
+
+const app = express();
+const port = 4000;
+
+const filesDirectory = `${__dirname}/dist`;
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+routes.forEach(({ path, file }) => {
+  app.get(path, function(req, res) {
+    res.sendFile(`${filesDirectory}${file}`);
+  });
+})
+
+app.use(function (req, res, next) {
+  res.status(404).sendFile(`${filesDirectory}/errorPages/404.html`);
+});
+
+app.listen(port);
